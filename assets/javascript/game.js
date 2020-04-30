fb2673eacaea10ce05f79af2aa1b19e09ae9198d
Samantha Taylor <staylor@mort.is>
2020-04-29 23:46:54 -0400

    Got the shoot button to count and randomly decide if its a goal or not. Both sides.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,13 +7,19 @@ let team2shots = document.querySelector("#teamtwo-numshots");
 let team2goals = document.querySelector("#teamtwo-numgoals");
 
 team1shoot.addEventListener("click", function () {
-    console.log("")
     let newteam1shoot = Number(team1shots.innerHTML) + 1;
-    team1shots.innerHTML = newteam1shoot
+    team1shots.innerHTML = newteam1shoot;
+    if (Math.random() >= .5)
+    return
+    let newteam1goals = Number(team1goals.innerHTML) + 1;
+    team1goals.innerHTML = newteam1goals;
 });
 
 team2shoot.addEventListener("click", function () {
-    console.log("")
     let newteam2shoot = Number(team2shots.innerHTML) + 1;
-    team2shots.innerHTML = newteam2shoot
+    team2shots.innerHTML = newteam2shoot;
+    if (Math.random() >= .5)
+    return
+    let newteam2goals = Number(team2goals.innerHTML) + 1;
+    team2goals.innerHTML = newteam2goals;
 });
