acdbba853d7cf9f1a58607b70e5794e484893709
Samantha Taylor <staylor@mort.is>
2020-05-01 23:45:04 -0400

    Made it so the reset shows a section in center for the winner. Results pending.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,6 +17,7 @@ let reset = document.querySelector("#reset-button");
 
 let hive = document.querySelector("#hive-button");
 let cayde = document.querySelector("#cayde");
+let winner = document.querySelector("#winner");
 
 team1shoot.addEventListener("click",
     function () {
@@ -25,11 +26,12 @@ team1shoot.addEventListener("click",
         let myAudioElement = new Audio('assets/media/vex_mythoclast.mp3');
         myAudioElement.addEventListener("canplaythrough", event => {
             myAudioElement.play();
-          });
+        });
         if (Math.random() >= .5) {
             goalsTeam1 += 1;
             team1goals.innerText = goalsTeam1;
         }
+        winner.classList.remove("winner");
     });
 
 team2shoot.addEventListener("click",
@@ -39,11 +41,12 @@ team2shoot.addEventListener("click",
         let myAudioElement = new Audio('assets/media/vex_mythoclast.mp3');
         myAudioElement.addEventListener("canplaythrough", event => {
             myAudioElement.play();
-          });
+        });
         if (Math.random() >= .5) {
             goalsTeam2 += 1;
             team2goals.innerText = goalsTeam2;
         }
+        winner.classList.remove("winner");
     });
 
 reset.addEventListener("click",
@@ -53,7 +56,20 @@ reset.addEventListener("click",
         let myAudioElement = new Audio('assets/media/get_back_out_there.mp3');
         myAudioElement.addEventListener("canplaythrough", event => {
             myAudioElement.play();
-          });
+        });
+
+        if (goalsTeam1 == goalsTeam2 == 0) {
+            winner.innerText = "GIVE US THE PRIMUS OR WE BLOW THE SHIP!";
+            }
+            
+        if (goalsTeam1 > goalsTeam2){
+            winner.innerText = "WINNER IS TEAM 1!";
+        }
+
+        else{
+            winner.innerText = "WINNER IS TEAM 2!";
+        }
+
 
         goalsTeam1 = 0;
         goalsTeam2 = 0;
@@ -66,13 +82,14 @@ reset.addEventListener("click",
         team2shots.innerText = shotsTeam2;
 
         cayde.classList.remove("cayde");
+        winner.classList.add("winner");
     });
 
-    hive.addEventListener("click",
+hive.addEventListener("click",
     function () {
         let myAudioElement = new Audio('assets/media/weve_woken_the_hive.mp3');
         myAudioElement.addEventListener("canplaythrough", event => {
             myAudioElement.play();
-          });
+        });
         cayde.classList.add("cayde");
     });
